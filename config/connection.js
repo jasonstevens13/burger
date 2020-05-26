baec8b7ed529e3dbeba5d305a05363478146dc88
jasonstevens13 <jasonstevens13@yahoo.com>
2020-05-26 13:58:29 -0400

    heroku connection code added to include jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,30 @@ const mysql = require("mysql");
 require('dotenv').config();
 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
 
-    // dotenv npm used to mask personal password
-    password: process.env.DB_PASSWORD,
-    database: "burgers_db"
-});
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+
+//     // dotenv npm used to mask personal password
+//     password: process.env.DB_PASSWORD,
+//     database: "burgers_db"
+// });
+
+const connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
